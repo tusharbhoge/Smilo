5f4b16ddeb16c3bafcc52a7df4d543c1eed78b4d
tushar <tusharbhoge80@gmail.com>
2026-06-27 19:39:50 +0530

    small bugs fixed in landing page

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -9,7 +9,8 @@ const Hero = () => {
     <section className="relative h-screen flex items-center overflow-hidden pt-20">
       {/* GRID BG  */}
       <div className="absolute inset-0 bg-linear-to-br from-background via-muted/5 to-primary/5">
-        <div className="absolute inset-0 bg-[linear-linear(to_right,#f1f5f9_1px,transparent_1px),linear-linear(to_bottom,#f1f5f9_1px,transparent_1px)] bg-size-[4rem_4rem] mask-[radial-linear(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] opacity-20"></div>
+        <div className="absolute inset-0 bg-[linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] opacity-20"></div>
+    
       </div>
 
       {/* linear ORBS */}
